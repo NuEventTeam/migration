9547ae2563aba1819c8099bf514f7c532495d2b7
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-01-26 04:03:16 +0600

    deleted at field

--- auth/000002_init_one_time_password_table.up.sql
+++ auth/000002_init_one_time_password_table.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE if not exists one_time_passwords (
       phone TEXT not null ,
       code TEXT not null ,
       type SMALLINT NOT NULL,
-      expires_at timestamp,
-      created_at timestamp DEFAULT CURRENT_TIMESTAMP
+      expires_at timestamp not null ,
+      created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+      deleted_at timestamp
 );
