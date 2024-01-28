27b75355b99e267f21bed6d693f67c9c165508fe
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-01-29 01:29:39 +0600

    event_id added in event managers

--- event/000009_init_event_managers_table.up.sql
+++ event/000009_init_event_managers_table.up.sql
@@ -3,6 +3,7 @@ create table  if not exists event_managers(
     title varchar(30) not null,
     user_id bigint references users(id),
     role_id bigint references event_roles(id),
+    event_id biginy reference events(id)
     created_at timestamp not null default now(),
     updated_at timestamp not null default now(),
     deleted_at timestamp
