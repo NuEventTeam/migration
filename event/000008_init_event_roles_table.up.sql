d6bd8af62adba0b4bfd9ea24d0af762687f76e13
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-02-09 23:53:46 +0600

    roles,permissions fixed

--- event/000008_init_event_roles_table.up.sql
+++ event/000008_init_event_roles_table.up.sql
@@ -1,6 +1,5 @@
 create table if not exists event_roles(
     id bigserial primary key,
-    permission_id bigint references event_permissions(id),
     name varchar(20) not null,
     created_at timestamp not null default now(),
     updated_at timestamp not null default now(),
