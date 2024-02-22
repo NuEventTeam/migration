98975c931681f15722b80f58074b20cf25d1809e
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-02-22 09:23:20 +0600

    user merged to event

--- event/000003_init_event_images_table.up.sql
+++ event/000003_init_event_images_table.up.sql
@@ -1,7 +1,7 @@
 create table if not exists event_images(
     id bigserial primary key,
     event_id bigint not null references events(id),
-    url varchar(50) not null unique,
+    url varchar(100) not null unique,
     created_at timestamp not null default now(),
     updated_at timestamp not null default now(),
     archived_at timestamp not null default  now(),
